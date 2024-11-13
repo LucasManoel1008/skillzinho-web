497cb599e5fdfd6ca59ba86c906780a1be1f3415
LucasManoel1008 <lucasgabrielsilvamanoel@gmail.com>
2024-11-13 11:45:41 -0300

    background color

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -7,21 +7,22 @@ import Tab from 'react-bootstrap/Tab';
 import MainContent from '../components/Home/MainContent.';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-regular-svg-icons';
+import Catalogo from '../components/Catalogo';
 function Home() {
  
   return (
     <div>
       <header className='text-center'>
-        <h1 >Skillzinho</h1>
+        <h1>Skillzinho</h1>
       </header>
       <div className={styles.content}>
         <Tab.Container id='left-tabs' defaultActiveKey='home'>
           <Row>
-              <Col sm={3}>
+              <Col sm={2}>
               <div className={styles.leftNavBar}>
                   <Nav className='flex-column'>
                     <Nav.Item>
-                      <Nav.Link eventKey='first'><FontAwesomeIcon icon={faUser} /></Nav.Link>
+                      <Nav.Link eventKey='user'><FontAwesomeIcon icon={faUser} /><span className='p-1'>Usuário</span></Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
                       <Nav.Link eventKey='home'>Home</Nav.Link>
@@ -38,7 +39,7 @@ function Home() {
                   <MainContent /> {/* Conteudo principal da página */}
                 </Tab.Pane>
                 <Tab.Pane eventKey='catalogo'>
-                  <h1>Catálogo</h1>
+                  <Catalogo />
                 </Tab.Pane>
               </Tab.Content>
             </Col>
